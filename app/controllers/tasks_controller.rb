f226fd968b8946785f69a93584b4de1381cd866f
rachel BJORK <rachel.bjork@edhec.com>
2016-08-09 16:40:24 +0200

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+ def index
+   @tasks = Task.all
+ end
+
+ def show
+   id = params[:id]
+   @task = Task.find(id)
+ end
+
+ def new
+   @task = Task.new
+ end
+
+ def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task, method: :get)
+  end
+
+  def edit
+   id = params[:id]
+   @task = Task.find(id)
+ end
+
+ def update
+   id = params[:id]
+   @task = Task.find(id)
+   @task.update(task_params)
+   redirect_to task_path(@task, method: :get)
+ end
+
+ def destroy
+   id = params[:id]
+   Task.find(id).delete
+   redirect_to tasks_path
+ end
+
+private
+ def task_params
+   params.require(:task).permit(:name, :description)
+ end
+end
